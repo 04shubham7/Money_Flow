7166cf6bd3fd348e4b204adfce5c1e26ccffd297
Shubham <shubham040711@gmail.com>
2024-12-28 03:07:04 +0530

    made xml of finance planning

--- app/src/main/java/com/example/money_flow/MainActivity.kt
+++ app/src/main/java/com/example/money_flow/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.money_flow
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.SignIn.SignIn
+import com.example.money_flow.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
@@ -18,5 +23,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, SignIn::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
---
+++ app/src/main/java/com/example/money_flow/User1.kt
@@ -0,0 +1,9 @@
+package com.example.money_flow
+
+data class User1(val name: String,
+                 val email: String,
+                 val username: String?,
+                 val password: String?
+){
+
+}
